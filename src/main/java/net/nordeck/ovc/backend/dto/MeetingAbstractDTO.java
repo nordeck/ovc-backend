7ce7597202b7264ce56da87e0e48d3d5653024ed
Wagner O. Wutzke <wagner.wutzke@nordeck.net>
2025-02-25 11:51:28 -0300

    feat: added started and started_ad columns to meeting

--- src/main/java/net/nordeck/ovc/backend/dto/MeetingAbstractDTO.java
+++ src/main/java/net/nordeck/ovc/backend/dto/MeetingAbstractDTO.java
@@ -80,4 +80,17 @@ public class MeetingAbstractDTO
     @JsonProperty("lobby_enabled")
     private boolean lobbyEnabled;
 
+    @Schema(description = "Whether this meeting has been started." +
+            "Relevant for instant meetings.")
+    @JsonProperty("started")
+    private boolean started;
+
+    @Schema(description = "The time UTC datetime in ISO-8601 format the meeting has been started. " +
+            "Needs to be converted by the client with its own zone offset. " +
+            "Relevant for INSTANT meeting.")
+    @JsonProperty("started_at")
+    @JsonSerialize(using = ZonedDateTimeSerializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Constants.DATE_TIME_ISO_8601_FORMAT)
+    private ZonedDateTime startedAt;
+
 }
--- src/main/java/net/nordeck/ovc/backend/entity/MeetingEntity.java
+++ src/main/java/net/nordeck/ovc/backend/entity/MeetingEntity.java
@@ -147,6 +147,12 @@ public class MeetingEntity {
     @Column(name = "excluded")
     private boolean excluded;
 
+    @Column(name = "started")
+    private boolean started;
+
+    @Column(name = "started_at")
+    private ZonedDateTime startedAt;
+
     @OneToMany(mappedBy = "meetingId", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<MeetingParticipantEntity> participants;
 
@@ -170,6 +176,8 @@ public class MeetingEntity {
                 .frequency(frequency)
                 .password(dto.getPassword())
                 .lobbyEnabled(dto.isLobbyEnabled())
+                .started(dto.isStarted())
+                .startedAt(dto.getStartedAt())
                 .build();
 
         setRecurrence(dto, entity);
